c4ff9c6f98a29cddc1307098db1d4f914b201777
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-06 12:05:32 -0500

    Update GeneralizedDeterminantCalculator.java

--- GeneralizedDeterminantCalculator.java
+++ GeneralizedDeterminantCalculator.java
@@ -5,8 +5,18 @@ public class GeneralizedDeterminantCalculator {
     /* n is the rows, columns of the n x n matrix */
 
     public static int calculate(int n, int[][] matrix) {
-
-
+        if (n == 1) {
+            return matrix[0][0];
+        } else if (n == 2) {
+            return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
+        } else {
+           // int[][] renovatedMatrix = renovateMatrix()
+           // return renovatedMatrix[0][0] * calculate(n - 1, new int[][] )
+        }
         return 0;
+    }
+
+    public static int[][] renovateMatrix(int[][] matrix, int j) {
+
     }
 }
